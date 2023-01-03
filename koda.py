1f0ea5c14d3c8e0e91a0061998159de238e979c4
Nikodin Sedlarevic <49947147+NikiFMF10@users.noreply.github.com>
2023-01-03 14:28:19 +0100

    Dodana funkcija, ki random izbere k tock

--- koda.py
+++ koda.py
@@ -53,3 +53,37 @@ def get_xyq(n):
     dy = b1 - ymin
     points = [(p[0]+dx, p[1]+dy) for p in points]
     return points
+
+# funkcija, ki random izbere k tock znotraj poligona
+def random_points_in_polygon(polygon, k):
+    areas = []
+    transforms = []
+    for t in triangulate(polygon):
+        areas.append(t.area)
+        (x0, y0), (x1, y1), (x2, y2),_ = t.exterior.coords
+        transforms.append([x1 - x0, x2 - x0, y2 - y0, y1 - y0, x0, y0])
+    tocke = []
+    for transform in random.choices(transforms, weights=areas, k=k):
+        x, y = [random.random() for _ in range(2)]
+        if x + y > 1:
+            p = Point(1 - x, 1 - y)
+        else:
+            p = Point(x, y)
+        tocke.append(affine_transform(p, transform))
+    return tocke
+
+
+
+if __name__ == "__main__":
+    n = 10
+
+    k = 1
+    if len(sys.argv) == 2:
+        n = int(sys.argv[1])
+    points = get_xyq(n)
+    polygon = Polygon(points)
+    tocke = random_points_in_polygon(polygon,k)
+    plt.plot([p[0] for p in points], [p[1] for p in points])
+    plt.show()
+    print("Zunanje tocke", points)
+    print(tocke)
