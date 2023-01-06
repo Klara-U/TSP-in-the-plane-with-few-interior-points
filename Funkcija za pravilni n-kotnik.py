b11152b8aa1db709b056cfbe320a4794a41c3e90
Nikodin Sedlarevic <49947147+NikiFMF10@users.noreply.github.com>
2023-01-06 14:09:20 +0100

    Naključne točke za pravilni n kotnik

---
+++ Funkcija za pravilni n-kotnik.py
@@ -0,0 +1,15 @@
+import math
+
+def polygon(sides, radius=1, rotation=0, translation=None):
+    one_segment = math.pi * 2 / sides
+
+    points = [
+        (math.sin(one_segment * i + rotation) * radius,
+         math.cos(one_segment * i + rotation) * radius)
+        for i in range(sides)]
+
+    if translation:
+        points = [[sum(pair) for pair in zip(point, translation)]
+                  for point in points]
+
+    return points
